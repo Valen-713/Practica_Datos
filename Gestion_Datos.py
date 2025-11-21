c7d282f84ffdf0c4bbeac7bec265464025e98640
Valentina Sandoval <Valen>
2025-11-21 14:40:33 -0500

    Feat(Practica_Datos): Se crea la clase hija Gestion_Datos donde se crea una lista ListaDatos la cual va a guardar cada persona que sea registrada. Adicionalmente se crean dos variables donde una es Agregar_Datos y estas queden dentro de la lista creada anteriormente y finalmente la segunda variable la cual es Consultar_Datos esta ayudara a consultar a la persona mediante su numero de cedula y asi tener sus datos

---
+++ Gestion_Datos.py
@@ -0,0 +1,28 @@
+#Importar las clases a utilizar
+from Datos import Datos
+from typing import List
+
+#Se cre una clase hija Gestion_Datos donde se crea una lista que va guardar cada persona que sea registrada
+class Gestion_Datos:
+        def __init__(self):
+                self._ListaDatos=[]
+                pass
+        @property
+        def ListaDatos(self)->List:
+                return self._ListaDatos
+        @ListaDatos.setter
+        def ListaDatos(self,ListaDatos:List):
+                self._ListaDatos=ListaDatos
+
+#se crea una variable para agregar los datos en la lista 
+        def Agregar_Datos(self,datos:Datos)->Datos:
+                self.ListaDatos.append(datos)
+
+#Se crea una variable la cual ayudara a consultar a la persona por su numero de cedula
+        def Consultar_Datos(self, Cedula):
+                for datos in self.ListaDatos:
+                        if datos.Cedula == Cedula:
+                                return datos
+        
+        
+        
