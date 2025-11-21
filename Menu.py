3010c853defa333b6347169ac76b4047debff35d
Valentina Sandoval <Valen>
2025-11-21 14:45:42 -0500

    Feat(Practica_Datos): Se crea un menu interactivo donde tiene 3 opciones: Agregar, Consultar y Salir. En la primera opcion se encuentra los datos de cada persona que se quiere registrar con los datos anteriormente creados. En la opcion 2 se digita el numero de cedula para consular los datos de la persona con ese numero de cedula y finalmente como opcion 3 para salir del menu y finalizar

---
+++ Menu.py
@@ -0,0 +1,46 @@
+#Importar las clases a utilizar
+from Datos import Datos
+from Gestion_Datos import Gestion_Datos
+#Instancias funciones
+GD = Gestion_Datos()
+Agregar = Datos()
+
+#Creacion de menu a utilizar
+while True:
+    print("1.Agregar")
+    print("2.Consultar")
+    print("3.Salir")   
+    opcion = input("Selecciona una opcion: ")
+
+#Se crea la opcion 1 donde se puede digitar los datos de cada persona
+    if opcion == "1":
+        Agregar.Nombre =str(input("Digite el nombre: "))
+        Agregar.Apellido =str(input("Digite el apellido: "))
+        Agregar.Edad =int(input("Digite la edad: "))
+        Agregar.Cedula =int(input("Digite el numero de cedula: "))
+        Agregar.Correo =input("Ingrese el correo: ")     
+        GD.Agregar_Datos(Agregar)
+        print("Datos agregados correctamente.\n")
+
+#Se crea la opcion 2 donde se digita el numero de cedula para hallar a esa persona con sus datos correspondientes
+    elif opcion == '2':
+        cedula = int(input("Ingrese la cedula a consultar: "))
+        resultado = GD.Consultar_Datos(cedula)
+
+#Se imprime los resultados
+        if resultado:
+            print("---- Datos encontrados ----")
+            print(f"Nombre: {resultado.Nombre}")
+            print(f"Apellido: {resultado.Apellido}")
+            print(f"Edad: {resultado.Edad}")
+            print(f"Correo: {resultado.Correo}")
+        else:
+            print("No se encontró un registro con esa cédula.")
+
+
+#Se crea la opcion 3, sale del menu        
+    elif opcion =="3": 
+        print("Salida")
+        break      
+    else:
+        print("Opcion no valida")
