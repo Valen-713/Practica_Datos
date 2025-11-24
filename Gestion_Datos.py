6f29a10740aefeab1ba75f251a51831893368473
Michelle Mogollon <Michi>
2025-11-24 08:19:04 -0500

    Feat(Practica_Datos): Se crean dos funciones nuevas, una permite modificar cualquier dato buscando mediante número de documento y la otra permite eliminar el registro completo con la cedula.

--- Gestion_Datos.py
+++ Gestion_Datos.py
@@ -7,9 +7,11 @@ class Gestion_Datos:
         def __init__(self):
                 self._ListaDatos=[]
                 pass
+
         @property
         def ListaDatos(self)->List:
                 return self._ListaDatos
+        
         @ListaDatos.setter
         def ListaDatos(self,ListaDatos:List):
                 self._ListaDatos=ListaDatos
@@ -23,18 +25,57 @@ class Gestion_Datos:
                 for datos in self.ListaDatos:
                         if datos.Cedula == Cedula:
                                 return datos
-                        
+
+#Se crea la funcion para modificar             
         def Modificar_Datos(self, cedula):
-                if len(self.listaDatos) == 0:
+#Cuenta los registros, si es = 0, arroja un mensaje        
+                if len(self.ListaDatos) == 0:
                         return "La lista está vacía."
-                for datos in self.listaDatos:
-                        if datos.cedula == cedula:
+#Recorre self.ListaDatos
+                for datos in self.ListaDatos:
+#Si datos.Cedula esta en cedula, encuentra los datos agregados anteriormente
+                        if datos.Cedula == cedula:
                                 print("Datos encontrados:")
-                                print(f"Nombre: {datos.nombre}")
-                                print(f"Apellido: {datos.apellido}")
-                                print(f"Edad: {datos.edad}")
-                                print(f"Correo: {datos.correo}")
-                                print(f"Cédula: {datos.cedula}")
+                                print(f"Nombre: {datos.Nombre}")
+                                print(f"Apellido: {datos.Apellido}")
+                                print(f"Edad: {datos.Edad}")
+                                print(f"Correo: {datos.Correo}")
+                                print(f"Cedula: {datos.Cedula}")
+#Menu que permite seleccionar lo que se desea modificar                                
+                                print("Selecciona que deseas modificar: ")
+                                print("1. Modificar nombre")
+                                print("2. Modificar apellido")
+                                print("3. Modificar edad")
+                                print("4. Modificar correo")
+                                print("5. Modificar cedula")
+                                opcion=input("Selecciona una opcion: ")
+#Segun la opcion sleccionada pide ingresar el dato actualizadp
+                                if opcion == "1":
+                                        datos.Nombre=input("Ingresa el nombre actualizado: ")
+                                elif opcion == "2":
+                                        datos.Apellido=input("Ingresa el apellido modificado: ")
+                                elif opcion == "3":
+                                        datos.Edad=input("Ingresa la edad actualizada: ")
+                                elif opcion == "4":
+                                        datos.Correo=input("Ingresa el correo actualizado: ")
+                                elif opcion == "5":
+                                        datos.Cedula=input("Ingresa la cedula actualizada: ")
+                                
+                                return "Dato modificado correctamente"
+#Se crea la opcion para borrar un registro
+        def Eliminar_Datos(self, cedula):
+                if len(self.ListaDatos) == 0:
+                        return "La lista está vacía."
+#Recorre y busca por cedula, si encunetra la cedula seleccionada, elimina el redgistro
+                for datos in self.ListaDatos:
+                        if datos.Cedula == cedula:
+                                self.ListaDatos.pop(self.ListaDatos.index(datos))
+                                return "Dato eliminado correctamente."
+
+                return "No se encontró la cédula."
+
+
+
                 
         
         
